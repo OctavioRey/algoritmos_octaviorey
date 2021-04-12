7d4edb19b03477dd1805ef5301c4936154d31868
Octavio <otocapitan@gmail.com>
2021-04-12 20:19:35 -0300

    Quicksort y Busqueda Binaria

--- Ejercicios_recursividad.py
+++ Ejercicios_recursividad.py
@@ -31,10 +31,57 @@ print (sumatoria_R(3))
 
 #Ejercicio Logaritmo
 
-def logaritmo(numero, base):
+''' def logaritmo(numero, base):
     if(numero / base < 1):
         return 0
     else:
         return 1 + logaritmo(numero/base , base)
 
-print (logaritmo(8, 2))
+print (logaritmo(8, 2)) '''
+
+
+#Ordenamiento Quicksort
+
+datos = [11, 3, 81, 7, 45, 20, 4, 5, 10, 22, 30, 25]
+
+def quicksort(vector, inicio, fin):
+    
+    primero = inicio
+    ultimo = fin-1
+    pivote = fin
+    while(primero < ultimo):
+        while(vector[primero] <= vector[pivote] and primero <= ultimo):
+            primero += 1
+        while(vector[ultimo]> vector[pivote] and ultimo >= primero):
+            ultimo -= 1
+
+        if(primero < ultimo):
+            vector[primero], vector[ultimo] = vector[ultimo], vector[primero]
+    if (vector[pivote] < vector[primero]):
+        vector[pivote], vector[primero] = vector[primero], vector[pivote]
+
+    if(inicio < ultimo):
+        quicksort(vector, inicio, primero -1)
+    if(primero < fin):
+        quicksort(vector, primero + 1, fin)
+
+
+# Busqueda Binaria Recursiva
+
+def busqueda_binaria_R(vector, buscado, inicio, fin):
+    
+    if(inicio <= fin):
+        medio = (inicio + fin ) // 2
+        if(vector[medio] == buscado ):
+            return medio
+        elif(vector[medio] < buscado):     
+            return busqueda_binaria_R(vector, buscado, medio+1, fin)
+        else:
+            return busqueda_binaria_R(vector, buscado, inicio, medio-1)
+    else:
+        return -1
+
+quicksort(datos, 0, (len(datos)-1))
+print (datos)
+print (busqueda_binaria_R(datos, 81, 0, len(datos)-1))
+
